dea728e099c55c439150d1d8f297b94f1b6332eb
Sam <sam@1485048.no-reply.drupal.org>
2016-04-04 23:39:03 +0800

    show last week.

--- src/Stats/Installs.php
+++ src/Stats/Installs.php
@@ -6,18 +6,26 @@ use stats\DataPointInterface;
 
 class Installs implements DataPointInterface {
 
+  protected $week = 0;
+
+  function __construct($week = NULL) {
+    if ($week) {
+      $this->week = $week;
+    }
+  }
+
   /**
    * {@inheritdoc}
    */
   public function getLabel() {
-    return 'Installs';
+    return 'Installs' . ($this->week ? " (-{$this->week} week)" : '');
   }
 
   /**
    * {@inheritdoc}
    */
   public function getDataPoint($moduleData) {
-    return $moduleData[0][1];
+    return $moduleData[$this->week][1];
   }
 
 }
